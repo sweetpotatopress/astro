70240294d98b4b7e4d7f5324c584f24bfa98109a
yamlynn <>
2026-05-01 22:22:58 -0500

    can save chart data to XDG_DATA_HOME now!

--- astro.c
+++ astro.c
@@ -11,15 +11,17 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. if not, see <https://www.gnu.org/licenses/> */
 
 #include <stdio.h>
+#include <math.h>
 #include <time.h>
 #include <unistd.h>
+#include <pwd.h>
 #include <swephexp.h>
 #include <ncurses.h>
-#include <math.h>
 #include <form.h>
 #include <panel.h>
 #include "astro.h"
 #include "city-search.c"
+
 
 void fieldbuffer_trim(FIELD *current, char *buffer)
 {
@@ -92,6 +94,145 @@ FORM *cdata_form, FIELD *cdata_field[])
 	}
 }
 
+void save_chart(struct tm *cdata, Location *loc)
+{
+	WINDOW *data_dir_win;
+	WINDOW *data_dir_subwin;
+	FORM *data_dir_form;
+	FIELD *data_dir_field[2];
+	char *tz_name = getenv("TZ");
+	
+	int ch = 0;
+	int starty, startx, maxy, maxx;
+	int height = 5;
+	int width = 30;
+	
+	getmaxyx(stdscr, maxy, maxx);
+	
+	starty = (maxy - height) / 2;
+	startx = (maxx - width) / 2;
+	
+	data_dir_win = newwin(height, width, starty, startx);
+	//only call derwin once
+	data_dir_subwin = 
+	derwin(data_dir_win, height - 2, width - 2, 0, 0);
+	
+	cbreak();
+	keypad(data_dir_win, TRUE);
+	clearok(data_dir_win, TRUE);
+	wclear(data_dir_win);
+	
+	data_dir_field[0] = new_field(1, 25, 2, 2, 0, 0);
+	set_field_back(data_dir_field[0], A_UNDERLINE);
+	field_opts_off(data_dir_field[0], O_STATIC);
+	field_opts_off(data_dir_field[0], O_AUTOSKIP);
+	
+	data_dir_field[1] = NULL;
+	
+	data_dir_form = new_form(data_dir_field);
+	set_form_win(data_dir_form, data_dir_win);
+	set_form_sub(data_dir_form, data_dir_subwin);
+	
+	touchwin(data_dir_win);
+	post_form(data_dir_form);
+	box(data_dir_win, 0, 0);
+	mvwaddstr(data_dir_win, 1, 1, "filename?");
+	wrefresh(data_dir_win);
+	
+	struct passwd *pw = getpwuid(getuid());
+	if (!pw) 
+	{
+		endwin();
+		perror("petpwuid data");
+		ERR_EXIT;
+	}
+	
+	char *data_dir = 
+	malloc(strlen(pw->pw_dir) + strlen("/.local/share") + 1);
+	if (!data_dir)
+	{
+		endwin();
+		perror("data_dir malloc");
+		ERR_EXIT;
+	}
+	
+	sprintf(data_dir, "%s/.local/share", pw->pw_dir);
+	
+	set_current_field(data_dir_form, data_dir_field[0]);
+	wrefresh(data_dir_win);
+	pos_form_cursor(data_dir_form);
+	
+	int done = 0;
+	while(!done && (ch = wgetch(data_dir_win)))
+	{
+		mode = INSERT;
+		switch (ch)
+		{
+			case '\n':
+				form_driver(data_dir_form, REQ_VALIDATION);
+				done = 1;
+				break;
+			default:
+				form_driver(data_dir_form, ch);
+				break;
+		}
+		wrefresh(data_dir_win);
+	}
+	
+	char *filename = field_buffer(data_dir_field[0], 0);
+	if (!filename || strlen(filename) == 0)
+	{
+		endwin();
+		wprintw(data_dir_win, "ERR: file has no name");
+		free(data_dir);
+		return;
+	}
+	size_t i = strlen(filename);
+	
+	//trim filename
+	while (i > 0 && filename[i - 1] == ' ')
+		filename[--i] = '\0';
+	
+	char filepath[PATH_MAX] = {0};
+	
+	snprintf(filepath, sizeof(filepath), "%s/astro/%s",
+	data_dir,
+	filename
+	);
+	
+	FILE *ifp = fopen(filepath, "w");
+	if (!ifp)
+	{
+		endwin();
+		perror("data_dir fopen");
+		ERR_EXIT;
+	}
+			
+	fprintf(ifp, "%d\n%d\n%d\n%d\n%d\n%s\n%f\n%f",
+		cdata->tm_year,
+		cdata->tm_mon,
+		cdata->tm_mday,
+		cdata->tm_hour,
+		cdata->tm_min,
+		tz_name,
+		loc->dlat,
+		loc->dlon
+		);
+		
+		fclose(ifp);
+		free(data_dir);
+		unpost_form(data_dir_form);
+		wclear(data_dir_win);
+		touchwin(data_dir_win);
+		wrefresh(data_dir_win);
+		free_form(data_dir_form);
+		
+		for (size_t j = 0; j < 2; ++j)
+			free_field(data_dir_field[j]);
+		delwin(data_dir_subwin);
+		delwin(data_dir_win);
+}
+
 void field_label(WINDOW *cdata_form_win, size_t i, int starty, int startx)
 {
 	const char *c_labels[] = {
@@ -153,10 +294,23 @@ void set_localtime(FIELD *cdata_field[], struct tm *cdata)
 	set_field_buffer(cdata_field[5], 0, buff);
 	
 }
+
+void validate_fields(WINDOW *cdata_form_win, FIELD *cdata_field[],
+FORM *cdata_form, struct tm *cdata, Location *loc)
+{
+	size_t i = 0;
+	//validates every field, in case user didnt hit enter
+	for (i = 1; i < 9; i++)
+	{
+		set_current_field(cdata_form, cdata_field[i]);
+		form_driver(cdata_form, REQ_VALIDATION);
+		field_to_member(cdata_form_win, cdata, loc, 
+		cdata_form, cdata_field);
+	}
+}
 	
 void ichart_data(struct tm *cdata, Location *loc)
 {
-
 	WINDOW *cdata_form_win;
 	FIELD *cdata_field[10];
 	FORM *cdata_form;
@@ -270,6 +424,11 @@ void ichart_data(struct tm *cdata, Location *loc)
 					case 27:
 						set_localtime(cdata_field, cdata);
 						break;
+					case 'w':
+						validate_fields(cdata_form_win, cdata_field,
+						cdata_form, cdata, loc);
+						save_chart(cdata, loc);
+						break;
 				}
 				break;
 			case INSERT:
@@ -313,15 +472,10 @@ void ichart_data(struct tm *cdata, Location *loc)
 		}
 		wrefresh(cdata_form_win);
 	}
-	//validates every field, in case user didnt hit enter
-	for (i = 1; i < 9; i++)
-	{
-		set_current_field(cdata_form, cdata_field[i]);
-		form_driver(cdata_form, REQ_VALIDATION);
-		field_to_member(cdata_form_win, cdata, loc, 
-		cdata_form, cdata_field);
-	}
 	
+	validate_fields(cdata_form_win, cdata_field,
+	cdata_form, cdata, loc);
+
 	unpost_form(cdata_form);
 	wclear(cdata_form_win);
 	touchwin(cdata_form_win);
