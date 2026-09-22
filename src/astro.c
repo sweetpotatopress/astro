dc92e7313829a9815363d33d4c38f8780e8ac31b
yam <yamlynn@noreply.codeberg.org>
2026-09-22 16:56:01 -0500

    defines for ALT + number hotkeys

--- src/astro.c
+++ src/astro.c
@@ -89,16 +89,16 @@ int main(int argc, char *argv[])
 	keypad(stdscr, TRUE);
 	
 	// alt + 1-0
-	define_key("\0331", 1001);
-	define_key("\0332", 1002);
-	define_key("\0333", 1003);
-	define_key("\0334", 1004);
-	define_key("\0335", 1005);
-	define_key("\0336", 1006);
-	define_key("\0337", 1007);
-	define_key("\0338", 1008);
-	define_key("\0339", 1009);
-	define_key("\0330", 1000);
+	define_key("\0331", ALT1);
+	define_key("\0332", ALT2);
+	define_key("\0333", ALT3);
+	define_key("\0334", ALT4);
+	define_key("\0335", ALT5);
+	define_key("\0336", ALT6);
+	define_key("\0337", ALT7);
+	define_key("\0338", ALT8);
+	define_key("\0339", ALT9);
+	define_key("\0330", ALT0);
 
 	init_pair(M_COLOR, COLOR_WHITE,  COLOR_BLACK);
 	init_pair(FIRE,    COLOR_RED,    COLOR_BLACK);
@@ -173,9 +173,9 @@ int main(int argc, char *argv[])
 			new_chart(cdata[ui->cc], pxx[ui->cc], ui, planet, zodiac);
 			doupdate();
 		}
-		if (ch >= 1000 && ch <= 1009)
+		if (ch >= ALT0 && ch <= ALT9)
 		{
-			int key = ch - 1000;
+			int key = ch - ALT0;
 			if (key == 0)
 				key = 10;
 			synastry(cdata, pxx, ui, planet, zodiac, key);
@@ -239,7 +239,6 @@ int main(int argc, char *argv[])
 				break;
 			case 'i':
 				in_cdata(cdata[ui->cc], pxx[ui->cc], ui, planet, zodiac, xdg_path);
-				
 				ecst_init(planet, cdata[ui->cc]->se);
 				new_chart(cdata[ui->cc], pxx[ui->cc], ui, planet, zodiac);
 				doupdate();
